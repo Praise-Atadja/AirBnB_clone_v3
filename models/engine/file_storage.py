c8d54309953cd7543989049c9d3980162dcff068
Praise-Atadja <p.atadja@alustudent.com>
2023-04-03 09:39:30 +0300

    update db_storage and file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID or None if not found """
+        
+        objects = self.all(cls)
+        class_id= "{}.{}".format(cls.__name__, id)
+        return objects.get(class_id)
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class."""
+    
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
