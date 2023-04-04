b7c2f4418f4ae043180d063aa454fb3b281e4e26
Praise-Atadja <p.atadja@alustudent.com>
2023-04-05 01:49:28 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
 """
-Start API .
+Start API
 """
-
-
-from flask import Flask, make_response, jsonify
-from os import getenv
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
-
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
-
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """ calls storage.close() """
+def teardown(self):
+    """ method to handle @app.teardown_appcontext """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ returns a JSON-formatted 404 status code response """
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def page_not_found(e):
+    """ handler for 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
 
 
-if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST'),
-            port=getenv('HBNB_API_PORT'),
-            threaded=True)
+if __name__ == '__main__':
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
+
+    app.run(host=host, port=port)
