20c7f306fca24000cadf73dac91f2384cd5b5ce0
Praise-Atadja <p.atadja@alustudent.com>
2023-04-07 14:52:00 +0300

    pep8

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -114,6 +114,7 @@ class TestFileStorage(unittest.TestCase):
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
 
+
 class TestGetCount(unittest.TestCase):
     @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_count_all(self):
